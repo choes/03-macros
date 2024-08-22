4b2b0ae9b3c8bb9b0c4d34884fac34137bc63f72
choes <choes.young@gmail.com>
2024-08-23 06:57:09 +0800

    feature: support generics for EnumFrom

--- examples/enum_macro.rs
+++ examples/enum_macro.rs
@@ -2,8 +2,8 @@ use macros::EnumFrom;
 
 #[allow(unused)]
 #[derive(Debug, EnumFrom)]
-enum Direction {
-    Up(DirectionUp),
+enum Direction<T> {
+    Up(DirectionUp<T>),
     Down,
     Left(u32),
     Right { a: u32 },
@@ -11,19 +11,19 @@ enum Direction {
 
 #[allow(unused)]
 #[derive(Debug)]
-struct DirectionUp {
-    speed: u32,
+struct DirectionUp<T> {
+    speed: T,
 }
 
 fn main() {
     // let up = Direction::Up(DirectionUp { speed: 42 });
-    let up: Direction = DirectionUp::new(42).into();
-    let left: Direction = 10.into();
+    let up: Direction<i32> = DirectionUp::new(42).into();
+    let left: Direction<i32> = 10.into();
     println!("Up: {:?}, Left: {:?}", up, left);
 }
 
-impl DirectionUp {
-    fn new(speed: u32) -> Self {
+impl<T> DirectionUp<T> {
+    fn new(speed: T) -> Self {
         Self { speed }
     }
 }
